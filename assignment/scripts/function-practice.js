bb3aa6dd3370a90a470d475a7bff67282450baaf
Mark Terry <mark.terry93@gmail.com>
2022-01-13 17:03:11 -0600

    Hello name Fixed

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -18,11 +18,10 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  name = "Mark Terry";
-  return name;
+  return 'Hello, ' + name;
 }
 
-console.log(helloName('Hello ') );
+console.log(helloName('Mark Terry') );
 // Remember to call the function to test
 
 
